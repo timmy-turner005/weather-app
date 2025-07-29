0fb535d5f15f604a4accdd423e1367237ae121db
timmy-turner005 <timitalented005@gmail.com>
2025-07-29 22:55:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Weather from "./components/weather/Index";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Weather />
+      </div>
+    </>
+  );
+}
+
+export default App;
